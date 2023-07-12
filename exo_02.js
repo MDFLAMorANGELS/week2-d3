980ae4ded1e69b201460e59e2bf409484021da2f
MDFLAMorANGELS <valentinmd78@gmail.com>
2023-07-12 23:27:01 +0200

    add exo

---
+++ exo_02.js
@@ -0,0 +1,25 @@
+const street1 = [3, 7, 8, 3, 6, 1];
+const street2 = [1, 4, 5, 8];
+
+function check(tab) {
+    const length = tab.length;
+    let result = 0;
+
+    for (let i = 0; i < length; i++) {
+        let sunsetView = true;
+
+        for (let j = i + 1; j < length; j++){
+            if (tab[j] >= tab[i]) {
+                sunsetView = false;
+                break;
+            }
+        }
+        if (sunsetView) {
+            result++;
+        }
+    }
+    return result;
+}
+
+console.log(check(street1));
+console.log(check(street2));
